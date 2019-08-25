e65fc8d3c51a70a84d2ef514ec6c08fe13326274
lconnery <lconnery@purdue.edu>
2019-08-25 16:38:24 -0400

    Small ViewController that retrieves user's speed and displays on screen. Also adds code for asking for permission to access user's location.

--- speed_test/AppDelegate.swift
+++ speed_test/AppDelegate.swift
@@ -17,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let viewController = ViewController()
+        
+        window?.rootViewController = viewController
+        
         return true
     }
 
--- speed_test/ViewController.swift
+++ speed_test/ViewController.swift
@@ -7,12 +7,98 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
+    let speed_display: UITextView = {
+       
+        let text_view = UITextView()
+        
+        text_view.text = "0 mph"
+        text_view.textAlignment = .center
+        text_view.font = UIFont(name: "HelveticaNeue-Thin", size: 50)
+        
+        text_view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return text_view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        format_view()
+
+        setup_lm()
+        
+        self.locationManager.startUpdatingLocation()
+        
+    
+        // add sub views here
+        self.view.addSubview(speed_display)
+        
+        format_subviews()
+    }
+    
+    func setup_lm() {
+       
+        locationManager.delegate = self
+        if NSString(string: UIDevice.current.systemVersion).doubleValue > 8 {
+            
+            locationManager.requestAlwaysAuthorization()
+        }
+
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didUpdateLocations locations: [CLLocation]) {
+        
+        guard let speed = manager.location?.speed else { return }
+       // speed_display.text = speed < 0 ? "No movement registered" : "\(speed) mph?"
+        
+        var mph: Int = Int(round(speed * 2.237))
+        
+        if (mph < 0) {
+            
+            mph = 0
+        }
+        
+        speed_display.text = "\(mph) mph"
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var speed: CLLocationSpeed = CLLocationSpeed()
+        speed = locationManager.location!.speed
+       
+    }
+    
+    private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if status != CLAuthorizationStatus.denied{
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func format_subviews() {
+        
+        // speed display
+        speed_display.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300).isActive = true
+        
+        speed_display.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        speed_display.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+
+        speed_display.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        speed_display.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+
+        
+    }
+    
+    func format_view() {
+        
+        self.view.backgroundColor = .white
     }
 
 
